de6cdb9f82a0cf21dfad0729029b523a363b3e0e
DaniBis <bisceanudaniel@gmail.com>
2017-10-03 22:57:59 +0300

    Have a look

--- src/App.js
+++ src/App.js
@@ -2,34 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+  class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
 
-class App extends Component {
-
-
-    componentDidMount() {
-        console.log("component mounted...");
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-  render() {
-    const Mystyle ={
-        position: 'fixed',
-        width: '100%',
-        height: '100%',
-        opacity: '0.6',
-        backgroundColor: 'black'
+    handleChange(event) {
+      this.setState({value: event.target.value});
     }
-    return (
-      <div className="App container-fluid">
-          <form>
-           <div style={Mystyle}>
-           <input type="text" value="Enter your id..."></input>
-           <input type="submit" value="submit"></input>
-           </div>
-          </form>
-      </div>
 
-    );
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+
+
+    render()
+      return (
+        <form style={MyContainer} onSubmit={this.handleSubmit}>
+          <label style={Mydiv}>
+            <input style={Myinput} type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input style={Myinput} type="submit" value="Submit" />
+        </form>
+      );
   }
-
 }
 export default App;
